009d0f2c2bf090239e5f2addaf5e9b53f41b5c85
tjdanqkr <tjdanqkr@gmail.com>
2024-09-10 10:48:53 +0900

    55

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import App2 from './components/App2';
-import Calc from './components/Calc';
+import Calc from './components/calc/Calc';
 
 //component .jsx
 function App() {
--- src/components/calc/ResultView.jsx
+++ src/components/calc/ResultView.jsx
@@ -1,5 +1,9 @@
-const ResultView = ({result})=>{
+import { useRecoilState, useRecoilValue } from "recoil";
+import { resultState } from "../../store/calc";
+
+const ResultView = ()=>{
     console.log("ResultView")
+    const result = useRecoilValue(resultState)
     return <h1>{result}</h1>
 }
 export default ResultView;
