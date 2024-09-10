45863d8cf24eb5c37dd5a2daa14b00ad6128e63a
tjdanqkr <tjdanqkr@gmail.com>
2024-09-10 16:30:54 +0900

    list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import App2 from './components/App2';
-import Calc from './components/calc/Calc';
+import Calc from './components/calc';
+import Person from './components/person';
 
 //component .jsx
 function App() {
-  
   return (
     <div className="App">
-      <Calc />
+      <Person />
     </div>
   );
 }
